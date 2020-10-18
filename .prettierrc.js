152a3edb1d84abde66b7c40181eba39ddf0dc97e
yanione <yanione@linecorp.com>
2020-10-18 09:00:57 +0900

    Update init resource files

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// prettier override configuration
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
