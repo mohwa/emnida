16318c1feeee92540aef7b64e77fdaac0ced933d
mohwa <yanione2@gmail.com>
2020-11-16 09:57:26 +0900

    Add test code about isEqual type

---
+++ tests/unit/type/isEqualAtStringFunction.spec.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line
+import { isEqualAtStringFunction } from '../../../lib';
+
+describe('isEqualAtStringFunction', () => {
+  it('should be return true or false when comparing function and function', () => {
+    // Given / When
+    const result1 = isEqualAtStringFunction(
+      () => console.log(1),
+      () => console.log(1)
+    );
+    const result2 = isEqualAtStringFunction(
+      () => console.log(1),
+      () => console.log(2)
+    );
+    // Then
+    expect(result1).toEqual(true);
+    expect(result2).toEqual(false);
+  });
+});
---
+++ tests/unit/type/isEqualAtStringSymbol.spec.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line
+import { isEqualAtStringSymbol } from '../../../lib';
+
+describe('isEqualAtStringSymbol', () => {
+  it('should be return true or false when comparing symbol and symbol', () => {
+    // Given / When
+    const result1 = isEqualAtStringSymbol(Symbol(3), Symbol(3));
+    const result2 = isEqualAtStringSymbol(Symbol(3), Symbol(4));
+    // Then
+    expect(result1).toEqual(true);
+    expect(result2).toEqual(false);
+  });
+});
