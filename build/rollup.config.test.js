fd7dc1e5490e6a3739dc3fd230a89655f28fb4a2
mohwa <yanione2@gmail.com>
2021-01-17 08:24:35 +0900

    Fix e2e test env

--- build/rollup.config.test.js
+++ build/rollup.config.test.js
@@ -1,11 +1,10 @@
 import { spawn } from 'cross-spawn';
 import serve from 'rollup-plugin-serve';
-// import htmlTemplate from 'rollup-plugin-generate-html-template';
 import html from '@rollup/plugin-html';
 import pkg from '../package.json';
 import { OUTPUT_PATH, mergeEntryConfig } from './utils';
 
-const IS_TEST = !!process.env.IS_TEST;
+const { E2E_ENV } = process.env;
 
 export default [
   mergeEntryConfig({
@@ -15,26 +14,17 @@ export default [
       name: pkg.name,
       plugins: [
         html(),
-        // htmlTemplate({
-        //   // template: 'examples/template.html',
-        //   // target: 'index.html',
-        // }),
-        // html2({
-        //   template: 'examples/template.html',
-        // }),
         serve({
           host: 'localhost',
           port: 9999,
           contentBase: OUTPUT_PATH,
           onListening: () => {
-            if (IS_TEST) {
-              console.log('Start e2e test');
-              const e2e = spawn('npm', ['run', 'e2e-ie11'], { stdio: 'inherit' });
-
-              //console.log(e2e);
+            if (E2E_ENV) {
+              console.log('Start E2E Test');
+              const e2e = spawn('npm', ['run', `nw:${E2E_ENV}`], { stdio: 'inherit' });
 
               e2e.on('close', code => {
-                console.log('Close e2e test', code);
+                console.log('Close E2E Test', code);
                 process.exit();
               });
             }
