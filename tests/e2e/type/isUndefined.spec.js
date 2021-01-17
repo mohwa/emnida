b74b56658c4ad4451fa6396d50c4086c3ce0719c
mohwa <yanione2@gmail.com>
2021-01-17 10:54:31 +0900

    Fix e2e test codes

--- tests/e2e/type/isUndefined.spec.js
+++ tests/e2e/type/isUndefined.spec.js
@@ -1,18 +1,37 @@
 import { assert } from 'chai';
 
 describe('isUndefined', function() {
-  it('should be return true if undefined type', (browser, done) => {
-    browser
-      .url(browser.launch_url)
-      .execute(
-        function() {
-          return window.emnida.type.isUndefined(undefined);
-        },
-        [],
-        function(ret) {
-          assert.equal(ret.value, true);
-        }
-      )
-      .end(done);
+  // eslint-disable-next-line no-undef
+  before((browser, done) => {
+    browser.url(browser.launch_url, () => done());
+  });
+
+  // eslint-disable-next-line no-undef
+  after((browser, done) => {
+    browser.end(() => done());
+  });
+
+  it('should be return true if undefined type', browser => {
+    browser.execute(
+      function() {
+        return window.emnida.type.isUndefined(undefined);
+      },
+      [],
+      function(ret) {
+        assert.equal(ret.value, true);
+      }
+    );
+  });
+
+  it('should be return false if string type', browser => {
+    browser.execute(
+      function() {
+        return window.emnida.type.isUndefined('test');
+      },
+      [],
+      function(ret) {
+        assert.equal(ret.value, false);
+      }
+    );
   });
 });
